b36cc170137afe3eeff2695e84ad92c16dd991aa
Enrique Bernardo Gonzalez Vazquez <ebgvdeveloper@gmail.com>
2022-11-22 14:21:56 -0600

    Se desarrollo el componente para el login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { UsersService } from "../userService/users.service";
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email: string;
+  password: string;
+  tituloAlerta: string='';
+
+  constructor(public userService: UsersService, public router: Router) {
+    // Inicializamos las variables 
+    this.email = "";
+    this.password = "";
+    // Verificar si ya hay un usuario logueado mediante la cookie
+    if(this.userService.getToken() != ''){
+      this.router.navigateByUrl('');
+    }
+   }
+
+   login() {
+    if(this.email != '' && this.password != ''){
+      if(this.email != '' || this.password != ''){
+        const user = {email: this.email, password: this.password};
+          this.userService.login(user).subscribe( data => {
+          // Informacion del usuario a loguear enviada
+          console.log(data);
+          // Guardar el token en una cookie
+          this.userService.setToken(data.token);
+          // Redireccionamos al dashboard
+          this.router.navigateByUrl('');
+        },
+        error => {
+          console.log(error);
+          Swal.fire('ACCESO DENEGADO', 'Datos erroneos o el usuario no existe', 'info');
+        });
+      } else {
+        Swal.fire('OPERACION DENEGADA', 'Porfavor complete el formulario!', 'info');
+      }
+    } else {
+      Swal.fire('OPERACION DENEGADA', 'Porfavor complete el formulario!', 'info');
+    }
+  }
+}
