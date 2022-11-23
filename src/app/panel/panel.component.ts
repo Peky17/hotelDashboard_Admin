2e14dadd5bf9b3849b1eee7266ef980a749cb079
Enrique Bernardo Gonzalez Vazquez <ebgvdeveloper@gmail.com>
2022-11-22 19:49:59 -0600

    Se corrigieron rutas y se agrregaron cookies para las sesiones

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { switchAll } from 'rxjs';
+import { UsersService } from '../userService/users.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-panel',
@@ -7,4 +11,20 @@ import { Component } from '@angular/core';
 })
 export class PanelComponent {
 
+   constructor(public userService: UsersService, public router: Router){
+    // Verificar si hay un usuario en sesion 
+    if(this.userService.getToken() != ""){
+    } else {
+      console.log("Tramposito");
+      this.router.navigateByUrl('login');
+    }
+  }
+
+  // Metodo para cerrar sesion
+  logout(){
+      Swal.fire('OPERACION DENEGADA', 'Porfavor complete el formulario!', 'info');
+      this.userService.deleteToken();
+      console.log("Ha cerrado sesion");
+      this.router.navigateByUrl('login');
+  }
 }
