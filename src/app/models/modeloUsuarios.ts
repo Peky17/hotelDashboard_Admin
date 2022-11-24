cb5c62d6cee199f35c1bd7b67d2fd5afd260b9dd
Enrique Bernardo Gonzalez Vazquez <ebgvdeveloper@gmail.com>
2022-11-24 00:35:29 -0600

    Subida al repositorio remoto

---
+++ src/app/models/modeloUsuarios.ts
@@ -0,0 +1,12 @@
+export class modeloUsuarios {
+
+    id: number;
+    username: string;
+    email: string;
+
+    constructor(){
+        this.id = 0;
+        this.username = "";
+        this.email = "";
+    }
+}
